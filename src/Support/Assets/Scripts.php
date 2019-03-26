60cf590aa20ae3cba2dd70f88cf97a2f83cc2a32
Rafael Audax <rafael@grupoaudax.com.br>
2019-03-26 20:59:26 +0000

    Change Support Scripts

--- src/Support/Assets/Scripts.php
+++ src/Support/Assets/Scripts.php
@@ -5,5 +5,25 @@ namespace GrupoAudax\AudaxPress\Support\Assets;
 
 class Scripts
 {
+    private $scripts = [];
 
+    public function add()
+    {
+        //
+    }
+
+    public function remove()
+    {
+        //
+    }
+
+    public function localize()
+    {
+        //
+    }
+
+    public function enqueue()
+    {
+        //
+    }
 }
